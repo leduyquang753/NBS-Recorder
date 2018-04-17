883811e3cbf6613caf70bdac4e44dc7b1fb1f768
Le Duy Quang <leduyquang753@gmail.com>
2018-04-17 21:14:04 +0700

    Added Forge auto updater.

--- src/main/java/cf/leduyquang753/nbsrecorder/Main.java
+++ src/main/java/cf/leduyquang753/nbsrecorder/Main.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "nbsrecorder", name = "NBS Recorder", version = "0.3.1", acceptedMinecraftVersions	 = "")
+@Mod(modid = "nbsrecorder", name = "NBS Recorder", version = "0.3.1", acceptedMinecraftVersions	 = "", updateJSON = "https://raw.githubusercontent.com/leduyquang753/NBS-Recorder/master/modUpdate.json")
 public class Main {
 	public static boolean recording = false;
 	public static boolean saving = false;
